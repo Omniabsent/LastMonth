292571df5b932881c18419f98db30833fcd35ebe
Júlia <julia.nihil@gmail.com>
2024-10-29 03:57:44 -0300

    generating 5x5 grid of last 30 days

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,10 @@
 class HomeController < ApplicationController
-  require 'faraday'
-  def index
-    lastmethod = "user.gettopalbums"
-    lastuser = "ayreonnaut"
-    @response = Faraday.get("http://ws.audioscrobbler.com/2.0/?method=#{lastmethod}&user=#{lastuser}")
+    require 'faraday'
+    def index
+        lastmethod = "user.gettopalbums"
+        lastuser = "ayreonnaut"
+        api_key = " "
+        period = "1month"
+        @response = Faraday.get("http://ws.audioscrobbler.com/2.0/?method=#{lastmethod}&user=#{lastuser}&period=#{period}&api_key=#{api_key}&format=json")
     end
 end
